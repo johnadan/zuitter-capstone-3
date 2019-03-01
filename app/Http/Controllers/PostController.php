6dcd5a860f40dac09146f0975dc3fe690346c3e3
johnadan <mclemadan@gmail.com>
2019-03-01 08:48:11 +0800

    db

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 
     public function getDashboard(){
         //$posts = Post::all();
-        $posts = Post::orderBy('created_at', 'ASC')->get();
+        $posts = Post::orderBy('created_at', 'DESC')->get();
         return view('layouts.dashboard', compact('posts'));
 
     }
