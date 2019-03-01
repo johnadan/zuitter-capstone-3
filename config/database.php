4c3165b157b7f8ff941445327cab1e3deda7a84c
johnadan <mclemadan@gmail.com>
2019-03-01 08:33:29 +0800

    db

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'http://sql12.freemysqlhosting.net/'),
+            'host' => env('DB_HOST', 'sql12.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'sql12281207'),
             'username' => env('DB_USERNAME', 'sql12281207'),
