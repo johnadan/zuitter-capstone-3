7c3b14c89925db543eea82f0ee1b9346dc447bc2
John McLem <mclemadan@gmail.com>
2021-02-19 11:14:10 +0800

    changed home icon redirect if user is an admin

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,5 +1,7 @@
 <nav class="nav navbar navbar-expand-lg sticky-top">
-    @guest
+
+{{-- @guest
+
   <a class="navbar-brand" href="{{ route('welcome') }}">
     <img src="{{ asset('images/zuitt.png') }}" width="50" height="50" alt="zuitt logo">
   </a>
@@ -16,8 +18,10 @@
         <a class="nav-link" href="{{ route('register') }}">Register <span class="sr-only">(current)</span></a>
       </li>
     </ul>
-  </div>    
-    @else
+  </div>   --}}
+
+  @if (Auth::user()->isAdmin == 0) 
+
   <a class="navbar-brand" href="/dashboard">
     <img src="{{ asset('images/zuitt.png') }}" width="50" height="50" alt="zuitt logo">
   </a>
@@ -55,8 +59,54 @@
       <input class="form-control mr-sm-2" type="search" placeholder="Enter a user" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form> --}}
-    @endguest
   </div>
+
+  {{-- @if (Auth::user()->isAdmin == 1) --}}
+  @elseif (Auth::user()->isAdmin == 1)
+
+  <a class="navbar-brand" href="/admindashboard">
+    <img src="{{ asset('images/zuitt.png') }}" width="50" height="50" alt="zuitt logo">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">    
+      <li class="nav-item active">
+        <a class="nav-link" href="/admindashboard">Hi, {{ ucfirst(Auth::user()->username) }}!</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">Logout</a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+          </form>
+      </li>
+    </ul>
+  </div>
+
+  @else
+  <a class="navbar-brand" href="{{ route('welcome') }}">
+    <img src="{{ asset('images/zuitt.png') }}" width="50" height="50" alt="zuitt logo">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ route('login') }}">Login <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="{{ route('register') }}">Register <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>  
+
+  @endif  
+  
 </nav>
 
 
