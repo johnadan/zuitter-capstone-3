5a73acb9248743ab03d813e52dae143b0bae55e8
John McLem Adan <mclemadan@gmail.com>
2021-03-01 18:04:25 +0800

    feat: :sparkles: added parsley for form validation in login page
    added parsley for form validation in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,28 @@
 
 
 @section('content')
+<!-- <div class="hidden bs-callout bs-callout-warning">
+  <h4>Oh snap!</h4>
+  <p>This form seems to be invalid :(</p>
+</div> -->
+
+<div class="d-none alert alert-warning" role="alert">
+  <!-- A simple warning alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like. -->
+  <h4>Oh snap!</h4>
+  <p>This form seems to be invalid :(</p>
+</div>
+
+<!-- <div class="hidden bs-callout bs-callout-info">
+  <h4>Yay!</h4>
+  <p>Everything seems to be ok :)</p>
+</div> -->
+
+<div class="d-none alert alert-info" role="alert">
+  <!-- A simple info alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like. -->
+  <h4>Yay!</h4>
+  <p>Everything seems to be ok :)</p>
+</div>
+
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-4"></div>
@@ -12,14 +34,14 @@
                 <div class="panel-heading"></div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                    <form class="form-horizontal" method="POST" action="{{ route('login') }}" data-parsley-validate="" id="login-form">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="form-control user-email" name="email" value="{{ old('email') }}" required autofocus required="" data-parsley-trigger="change">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -56,9 +78,10 @@
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <div class="g-recaptcha" data-sitekey="6LftXGgaAAAAANpNyRyPmViitf2IEE7vxQBhKi2M"></div>
-                                <button type="submit" class="button tuitt-button is-btn-red text-white">
+                                <!-- <button type="submit" class="button tuitt-button is-btn-red text-white">
                                     Login
-                                </button>
+                                </button> -->
+                                <input type="submit" class="button tuitt-button is-btn-red text-white" value="Login">
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Forgot Your Password?
@@ -71,4 +94,46 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+
+function validate()
+{
+    $('#login-form').parsley().validate();
+}
+$(document).ready(function(){
+// $(".text1").attr("data-parsley-required","true");
+    $(".user-email").attr("data-parsley-type","email");
+
+    $('#login-form').parsley().on('form:validated', function() {
+        
+        var errorCount = $("#login-form .parsley-error").length;
+        
+        if(errorCount === 0){
+            $(".validateResult").html("Validated");
+        } else {
+            $(".validateResult").html("Invalid");
+        }
+    
+    });
+    
+    $('#login-form').parsley().validate();
+});
+
+// $(function () {
+//     $('#login-form').parsley().on('field:validated', function() {
+//     var ok = $('.parsley-error').length === 0;
+//     // $('.bs-callout-info').toggleClass('hidden', !ok);
+//     $('.alert-info').toggleClass('d-none', !ok);
+//     // $('.bs-callout-warning').toggleClass('hidden', ok);
+//     $('.alert-warning').toggleClass('d-none', ok);
+//   })
+//   .on('form:submit', function() {
+//     //return false; // Don't submit form for this demo
+//     return true; 
+//   });
+// });    
+
+</script>
+
 @endsection
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -43,3 +43,6 @@
         crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
         crossorigin="anonymous"></script> -->
+
+<!-- parsleyjs -->
+<script type="text/javascript" src="{{ asset('js/parsley.min.js') }}"></script>
